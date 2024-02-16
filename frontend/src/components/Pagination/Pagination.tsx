8e5c7c547fa523d39a7d66b9e8fb69b85b210e8c
Maxim Predybailo <predmaxim@ya.ru>
2024-02-17 00:43:48 +0500

    feat(frontend): add Pagination

---
+++ frontend/src/components/Pagination/Pagination.tsx
@@ -0,0 +1,30 @@
+import styles from './Pagination.module.scss';
+import {PostQueryProps} from '../../libs/types.ts';
+import {Button} from '../../components';
+
+type PaginationProps = {
+  page: PostQueryProps['page'],
+  pages: number,
+  onClick: (page: number) => void
+}
+
+export const Pagination = ({page: activePage, pages, onClick}: PaginationProps) => {
+  return (
+    <div className={styles.Pagination}>
+      {
+        (Array.from({length: pages}, (_, i) => ++i))
+          .map((page) => {
+            return (
+              <Button
+                className={`${styles.Pagination__item} ${activePage === page ? styles.active : ''}`}
+                onClick={() => onClick(page)}
+                key={page}
+              >
+                {page}
+              </Button>
+            );
+          })
+      }
+    </div>
+  );
+};
