e45571e9d3cc97394fc03a286df7c131d4b13370
Maxim Predybailo <predmaxim@ya.ru>
2024-02-17 00:45:49 +0500

    feat(frontend): add PostSort

---
+++ frontend/src/components/PostSort/PostSort.tsx
@@ -0,0 +1,37 @@
+import styles from './PostSort.module.scss';
+import {DIRECTION, SORT_BY, SORT_BY_DEFAULT} from '../../libs/constants';
+import {ChangeEvent, useState} from 'react';
+import {PostQueryProps} from '../../libs/types';
+import {genFirstLetterUpperCase} from '../../libs/helpers';
+
+type PostSortProps = PostQueryProps & {
+  onChange: (sortValue: string[]) => void
+}
+
+export const PostSort = ({onChange}: PostSortProps) => {
+  const [selectedValue, setSelectedValue] = useState(`${SORT_BY_DEFAULT} ${DIRECTION}`);
+
+  const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedValue(e.currentTarget.value);
+    onChange(e.currentTarget.value.split(' '));
+  };
+
+  return (
+    <select
+      className={`${styles.PostSort} input`}
+      onChange={onChangeHandler}
+      value={selectedValue}
+    >
+      {/*<option value="">- Sort By-</option>*/}
+      {Object.values(SORT_BY).map((sortValue) => {
+        return Object.values(DIRECTION).map((directionValue) => {
+          return (
+            <option value={`${sortValue} ${directionValue}`} key={sortValue + directionValue}>
+              {genFirstLetterUpperCase([sortValue, directionValue])}
+            </option>
+          );
+        });
+      })}
+    </select>
+  );
+};
---
+++ frontend/src/components/PostSort/index.ts
@@ -0,0 +1,1 @@
+export {PostSort} from './PostSort';
