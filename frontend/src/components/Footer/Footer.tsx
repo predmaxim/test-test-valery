b8cbd512778366af98e7b3d139b3265a80a4fc4d
Maxim Predybailo <predmaxim@ya.ru>
2024-02-17 00:40:55 +0500

    feat(frontend): add Footer

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.scss';
+
+export const Footer = () => {
+  return (
+    <footer className="Footer">
+      <div className="container">
+        &copy; Made with ❤️ <a href="https://github.com/predmaxim" target="_blank" rel="noreferrer">PredMaxim</a>
+      </div>
+    </footer>
+  );
+};
